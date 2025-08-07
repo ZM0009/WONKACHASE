431cf2a630a0b9a342162bf1fb3e83b646b14af4
ZM0009 <zmcewen@gmail.com>
2025-08-07 17:33:14 +1000

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-// Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyByo4Mg-uuVWlrw9DehBbE71nKAW0wO7MA",
+  authDomain: "gridgame-365b3.firebaseapp.com",
+  databaseURL: "https://gridgame-365b3-default-rtdb.firebaseio.com",
+  projectId: "gridgame-365b3",
+  storageBucket: "gridgame-365b3.firebasestorage.app",
+  messagingSenderId: "679304821204",
+  appId: "1:679304821204:web:ca6464eb9d0b90c9cefe41"
 };
 
 firebase.initializeApp(firebaseConfig);
